783f38de15847b04a6d59797d363a37945805ea0
fischermatte@gmx.net <fischermatte@gmx.net>
2013-05-03 01:46:07 +0200

    added bootstrap theme

--- cost-client-angular/app/js/controllers.js
+++ cost-client-angular/app/js/controllers.js
@@ -2,18 +2,18 @@
 
 /* Controllers */
 
-function CostListCtrl($scope,$http) {
+function CostListCtrl($scope, $http) {
     $scope.filterOptions = {
         filterText: "",
         useExternalFilter: true
     };
     $scope.pagingOptions = {
-        pageSizes: [5, 10, 15],
-        pageSize: 10,
+        pageSizes: [20, 50, 100],
+        pageSize: 20,
         totalServerItems: 0,
         currentPage: 1
     };
-    $scope.setPagingData = function(data, page, pageSize){
+    $scope.setPagingData = function(data, page, pageSize) {
         var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
         $scope.myData = pagedData;
         $scope.pagingOptions.totalServerItems = data.length;
@@ -21,20 +21,20 @@ function CostListCtrl($scope,$http) {
             $scope.$apply();
         }
     };
-    $scope.getPagedDataAsync = function (pageSize, page, searchText) {
-        setTimeout(function () {
+    $scope.getPagedDataAsync = function(pageSize, page, searchText) {
+        setTimeout(function() {
             var data;
             if (searchText) {
                 var ft = searchText.toLowerCase();
-                $http.get('data/costs.json').success(function (largeLoad) {
+                $http.get('data/costs.json').success(function(largeLoad) {
                     data = largeLoad.filter(function(item) {
                         return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
                     });
-                    $scope.setPagingData(data,page,pageSize);
+                    $scope.setPagingData(data, page, pageSize);
                 });
             } else {
-                $http.get('data/costs.json').success(function (largeLoad) {
-                    $scope.setPagingData(largeLoad,page,pageSize);
+                $http.get('data/costs.json').success(function(largeLoad) {
+                    $scope.setPagingData(largeLoad, page, pageSize);
                 });
             }
         }, 100);
@@ -42,10 +42,10 @@ function CostListCtrl($scope,$http) {
 
     $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
 
-    $scope.$watch('pagingOptions', function () {
+    $scope.$watch('pagingOptions', function() {
         $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
     }, true);
-    $scope.$watch('filterOptions', function () {
+    $scope.$watch('filterOptions', function() {
         $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
     }, true);
 
