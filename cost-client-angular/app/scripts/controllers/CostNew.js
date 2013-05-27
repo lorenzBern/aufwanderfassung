ab6756a0b16076c335e43e7aa77785cf50549df8
fischermatte@gmx.net <fischermatte@gmx.net>
2013-05-28 00:15:33 +0200

    added intellij project files

---
+++ cost-client-angular/app/scripts/controllers/CostNew.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('costsApp')
+  .controller('CostNewCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
